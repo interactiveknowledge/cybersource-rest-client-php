a1670aa07c55f4150cc822a009b5a3b76dc589ee
Philip Curley <philip@interactiveknowledge.com>
2023-02-06 08:33:02 -0500

    Remove autoload.php

--- lib/Authentication/Jwt/JsonWebTokenHeader.php
+++ lib/Authentication/Jwt/JsonWebTokenHeader.php
@@ -9,7 +9,7 @@ use CyberSource\Authentication\Core\AuthException as AuthException;
 use Firebase\JWT\JWT as JWT;
 use CyberSource\Logging\LogFactory as LogFactory;
 
-require_once 'vendor/autoload.php';
+// require_once 'vendor/autoload.php';
 
 class JsonWebTokenHeader 
 {
